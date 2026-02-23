fc203b91320ac4fd557f822495d59252a967c87c
Gerber Rogelio Díaz Santay <rogerdisan18@gmail.com>
2026-02-22 22:34:59 -0600

    Logica completa yippie x2

--- Program.cs
+++ Program.cs
@@ -42,3 +42,101 @@ double total = monto;
 
 switch (cliente)
 {
+    case 1: // Estudiante
+        if (pago == 1)
+        {
+            descuento = monto * 0.10;
+        }
+        else if (pago == 2)
+        {
+            descuento = monto * 0.07;
+        }
+        else if (pago == 3)
+        {
+            descuento = monto * 0.05;
+        }
+        break;
+
+    case 2: // Docente
+        if (pago == 1)
+        {
+            descuento = monto * 0.15;
+        }
+        else if (pago == 2)
+        {
+            descuento = monto * 0.10;
+        }
+        else if (pago == 3)
+        {
+            descuento = monto * 0.08;
+        }
+        break;
+
+    case 3: // Administrativo
+        if (pago == 1)
+        {
+            descuento = monto * 0.20;
+        }
+        else if (pago == 2)
+        {
+            descuento = monto * 0.12;
+        }
+        else if (pago == 3)
+        {
+            descuento = monto * 0.10;
+        }
+        break;
+
+    case 4: // Externo
+        if (pago == 1)
+        {
+            descuento = monto * 0.05;
+        }
+        else if (pago == 2)
+        {
+            descuento = monto * 0.02;
+        }
+        break;
+
+    default:
+        Console.WriteLine("Cliente no valido");
+        return;
+}
+
+if (tieneCupon == 'S' && codigoCupon.Length >= 2)
+{
+    char primera = codigoCupon[0];
+    char ultima = codigoCupon[codigoCupon.Length - 1];
+
+    if (primera == 'U' && char.IsDigit(ultima))
+    {
+        int ultimoNumero = int.Parse(ultima.ToString());
+        if (ultimoNumero % 2 == 0)
+        {
+            descuento += monto * 0.05;
+        }
+        else
+        {
+            Console.WriteLine("Cupon invalido");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Cupon invalido");
+    }
+}
+
+if (fraude == 2 || fraude == 3)
+{
+    Console.WriteLine("ALERTA: Posible fraude detectado");
+    descuento = 0;
+    recargo = monto * 0.10;
+}
+
+total = monto - descuento + recargo;
+
+Console.WriteLine("\n=== FACTURA ===");
+Console.WriteLine("Monto base: " + monto);
+Console.WriteLine("Descuento: " + descuento);
+Console.WriteLine("Recargo: " + recargo);
+Console.WriteLine("TOTAL: " + total);
